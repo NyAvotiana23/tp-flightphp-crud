af0eb922e220568a31eb97608de31c79ca63c57c
InsAnjara <156006873+InsAnjara@users.noreply.github.com>
2025-07-07 20:38:56 +0300

    Fixes

--- ws/controllers/EtudiantController.php
+++ ws/controllers/EtudiantController.php
@@ -17,7 +17,6 @@ class EtudiantController {
 
     public static function create() {
         $model = new Etudiant();
-        $model = new Etudiant();
         $data = Flight::request()->data;
 
         $id = $model->create($data);
@@ -27,16 +26,12 @@ class EtudiantController {
 
     public static function update($id) {
         $model = new Etudiant();
-
-        $model = new Etudiant();
         $data = Flight::request()->data;
         $model->update($id, $data);
         Flight::json(['message' => 'Étudiant modifié']);
     }
 
     public static function delete($id) {
-        $model = new Etudiant();
-
         $model = new Etudiant();
         $model->delete($id);
         Flight::json(['message' => 'Étudiant supprimé']);
