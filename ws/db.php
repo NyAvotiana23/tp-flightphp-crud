61e15684ae530d3edb4cb189bd6da869c06626a9
JoanelRak <joanelrak01@gmail.com>
2025-07-08 01:00:18 +0300

    Client

--- ws/db.php
+++ ws/db.php
@@ -1,12 +1,10 @@
 <?php
 
 function getDB() {
-    $host = '127.0.0.1';
-    $dbname = 'db_s2_etu003115';
+    $host = 'localhost';
+    $dbname = 'finance';
     $username = 'root';
     $password = '';
-
-
     try {
         return new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password, [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
