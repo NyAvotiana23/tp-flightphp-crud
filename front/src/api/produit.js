26f565b22b291a119edd0c27778b520214fe8e96
JoanelRak <joanelrak01@gmail.com>
2025-07-08 11:05:55 +0300

    Produits

--- front/src/api/produit.js
+++ front/src/api/produit.js
@@ -7,15 +7,14 @@ ajax('GET', '/type-partenaire', null, res => {
 function createCard(produit) {
         return `
             <div class="bg-white p-6 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
-                <h3 class="text-h4 font-semibold text-custom-black">${produit.nom_produit}</h3>
+                <h3 class="text-h4 font-semibold text-custom-black">${produit.type_partenaire} / ${produit.nom_partenaire}</h3>
                 <p class="text-custom-sm text-gray-600 mb-2">Statut: ${produit.statut_produit}</p>
-                <p class="text-base text-custom-black mb-4">${produit.description_produit || 'Pas de description.'}</p>
+                <p class="text-base text-custom-black mb-4">${produit.description_partenaire || 'Pas de description.'}</p>
                 <div class="border-t pt-4">
                     <h4 class="text-h5 font-medium text-custom-black">Informations</h4>
-                    <p class="text-custom-sm text-gray-600">Créé le: ${new Date(produit.date_creation).toLocaleDateString()}</p>
                     ${produit.commentaire ? `<p class="text-custom-sm text-gray-600 mt-2">${produit.commentaire}</p>` : ''}
                 </div>
-                <button class="mt-4 w-full bg-custom-purple-primary hover:bg-custom-purple-secondary text-white font-medium py-2 px-4 rounded-md text-base">
+                <button  class="mt-4 w-full bg-custom-purple-primary hover:bg-custom-purple-secondary text-white font-medium py-2 px-4 rounded-md text-base">
                     Voir Détails
                 </button>
             </div>
@@ -30,6 +29,7 @@ function createCard(produit) {
                 container.innerHTML = `<p class="text-gray-500 col-span-3">Aucun produit disponible.</p>`;
                 return;
             }
+            console.log(data);
             container.innerHTML = data.map(produit => createCard(produit)).join('');
         }, (err) => {
             container.innerHTML = `<p class="text-red-500 col-span-3">Erreur lors du chargement des produits.</p>`;
